04416628eebabf0b0f9296bb39bc7132095156d5
Gabe Calderon <john_gabriel_calderon@dlsu.edu.ph>
2026-07-02 19:03:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: "https://gabe-0017.github.io",
+  base: "/CSARCH2-Group-6/",
 });
